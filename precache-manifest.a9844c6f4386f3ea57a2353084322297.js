51a1eb5856502deb08edf84fa49910e3ebf1b7e5
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-06-08 04:26:09 +0200

    Updates

---
+++ precache-manifest.a9844c6f4386f3ea57a2353084322297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfe333ce5b70b4a0d8d1423b2dcda0f",
+    "url": "/VU_Interactive_map/index.html"
+  },
+  {
+    "revision": "ff4b2fb2bef135bcb6b9",
+    "url": "/VU_Interactive_map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ff8627fda50015ab8c8",
+    "url": "/VU_Interactive_map/static/js/2.eaf95f99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VU_Interactive_map/static/js/2.eaf95f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4b2fb2bef135bcb6b9",
+    "url": "/VU_Interactive_map/static/js/main.cdae938a.chunk.js"
+  },
+  {
+    "revision": "670636fce2a5516965e0",
+    "url": "/VU_Interactive_map/static/js/runtime-main.81a10bc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VU_Interactive_map/static/media/logo.5d5d9eef.svg"
+  }
+]);
