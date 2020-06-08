0796ac6b2598803861a2c2589e2693131ddf25b9
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-06-08 08:08:15 +0200

    Updates

---
+++ precache-manifest.95ae482ed288b4b55a71a2de0e8f39e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d172347b5bb0630e77a52926249872f",
+    "url": "/VU_Interactive_map/index.html"
+  },
+  {
+    "revision": "f9a2bfd0ff8a4ee04f6f",
+    "url": "/VU_Interactive_map/static/css/main.11538b4e.chunk.css"
+  },
+  {
+    "revision": "d93fdd65d340f1df8241",
+    "url": "/VU_Interactive_map/static/js/2.80a1895c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VU_Interactive_map/static/js/2.80a1895c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a2bfd0ff8a4ee04f6f",
+    "url": "/VU_Interactive_map/static/js/main.46649c93.chunk.js"
+  },
+  {
+    "revision": "670636fce2a5516965e0",
+    "url": "/VU_Interactive_map/static/js/runtime-main.81a10bc5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VU_Interactive_map/precache-manifest.9d8102312bee9d8d4ac7397651c2e81a.js"
+  "/VU_Interactive_map/precache-manifest.95ae482ed288b4b55a71a2de0e8f39e5.js"
 );
 
 self.addEventListener('message', (event) => {
